b0e516618e3c6b45182176a50def3b61532cf1bf
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-04 22:23:51 -0300

    aula 04-03-2026

---
+++ 04-03-2026.py
@@ -0,0 +1,17 @@
+
+fila = []
+def enfileirar():
+
+    fila.append("Ana")
+    fila.append("BRuno")
+    fila.append("Carla")
+    fila.append("Diego")
+    print(fila)
+    print(fila[0])
+
+def desenfileirar():
+    saiu = fila[0]
+    fila.pop(0)
+
+
+
